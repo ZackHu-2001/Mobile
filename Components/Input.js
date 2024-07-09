cd09f2138a09bbd3c7dfe18493cbd8b4524df9b6
zackhu's macbook air <zackhu2001@gmail.com>
2024-07-09 14:40:39 -0700

    Add modal

--- Components/Input.js
+++ Components/Input.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextInput, Text, View, Modal } from 'react-native';
+import { TextInput, Text, View, Modal, Button } from 'react-native';
 
 const Input = ({ handleInputData, modalVisibility }) => {
   const [text, setText] = useState('');
@@ -7,25 +7,31 @@ const Input = ({ handleInputData, modalVisibility }) => {
 
   return (
     <Modal animationType="slide" visible={modalVisibility}>
-      <View style={{ width: 'full', justifyContent: 'center', alignContent:'center' }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center', flex: '1' }}>
 
         <TextInput
           placeholder="Type here to translate!"
           onChangeText={(text) => {
             setText(text);
-            handleInputData(text);
-            setShowThankYou(false);
+            // handleInputData(text);
+            // setShowThankYou(false);
           }}
           value={text}
           autoFocus={true}
           onBlur={() => setShowThankYou(true)}
           onFocus={() => setShowThankYou(false)}
         />
-        {showThankYou && <Text>Thank you</Text>}
+        <Button title='Confirm' style={{ width: '33%' }}
+          onPress={() => {
+            handleInputData(text);
+          }} />
+        {/* {showThankYou && <Text>Thank you</Text>} */}
       </View>
     </Modal>
   );
 };
+
+// fkexL 1;
 
 
 export default Input;
