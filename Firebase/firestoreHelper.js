b6e4b4cbc7ca49a78abe588bb02da37b362c5ba7
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2024-07-23 15:43:05 -0700

    submit lab 4

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -1,12 +1,12 @@
 import { db } from "./firebaseSetup";
-import { collection, addDoc, deleteDoc, doc } from "firebase/firestore";
+import { collection, addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
 
 export async function writeToDB(goal) {
     try {
         await addDoc(collection(db, "goals"), {
             goal: goal,
+            isWarning: false,
         });
-        console.log(goal)
     }
     catch (err) {
         console.log(err)
@@ -16,6 +16,17 @@ export async function writeToDB(goal) {
 export async function deleteFromDB(id) {
     try {
         await deleteDoc(doc(db, "goals", id));
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export async function updateIswarning(id, isWarning) {
+    try {
+        await updateDoc(doc(db, "goals", id), {
+            isWarning: isWarning,
+        });
     }
     catch (err) {
         console.log(err)
